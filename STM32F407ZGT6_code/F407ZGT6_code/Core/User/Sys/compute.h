33538453f2215bba29c036ce759a0030173f2b7a
Yufei Zhao <CHN_PEK_ZYF@163.com>
2026-04-08 22:36:47 +0800

    调参但结果有限

--- STM32F407ZGT6_code/F407ZGT6_code/Core/User/Sys/compute.h
+++ STM32F407ZGT6_code/F407ZGT6_code/Core/User/Sys/compute.h
@@ -23,18 +23,18 @@ extern UART_HandleTypeDef huart1;
 #define CTRL_DT   0.02f   // 20ms, 50Hz
 
 /* 姿态 PID（力矩环，快） */
-#define PITCH_KP   0.01f
+#define PITCH_KP   0.02f
 #define PITCH_KI   0.0000000005f/CTRL_DT
 #define PITCH_KD   0.0005f/CTRL_DT
 
-#define SPEED_KP 0.01f
+#define SPEED_KP 0.05f
 #define SPEED_KI 0.0f/CTRL_DT
 #define SPEED_KD 0.0f/CTRL_DT
 
 /* 限幅 */
 #define TORQUE_LIMIT   0.11f
 #define XREF_LIMIT     20.0f   // ±20mm
-#define STAND_Y_REF   -200.0f   // 稳定站立高度
+#define STAND_Y_REF   -150.0f   // 稳定站立高度
 
 
 #define PITCH_LIMIT        20.0f   // 俯仰角超限±20度 → 直接停机（核心保护）
